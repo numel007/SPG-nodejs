0c2b94df92ab7eda6b7463ecf67072336b5a2235
benchan777 <benchan777@gmail.com>
2021-07-06 10:27:03 -0700

    add functions to verify token and retrieve new access token

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,8 @@
+require("dotenv").config();
+const request = require("request");
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+
 const ranString = (length) => {
     str = "";
     let possible =
@@ -9,6 +14,49 @@ const ranString = (length) => {
     return str;
   }
 
+// Token verification
+const verifyToken = (req, res, next) => {
+    // const token = req.cookies.accessToken
+    const accessToken = "BQDK99qlexjiWMzGYl1CTT1ipq5KD7tH_G6-jEsY5dxWlSpehCH_2x8bJIWso-2w4-KWMuAED2-QJBmZSb-1m-_fvBakO1z_RYs8TSynOnydEMZTJQqu5eL0fgHocGz_OOV_vORW0dYkAL3FAY_JFCr3bu9iFwHECiwyKavv5UYRflA8LS6YForGwfjDBQz2zXT5Hs37S-S3gdxy"
+    const refreshToken = req.cookies.refreshToken
+    let authOptions = {
+        url: "https://api.spotify.com/v1/me",
+        headers: { Authorization: "Bearer " + accessToken },
+        json: true
+    };
+
+    request.get(authOptions, (err, res, body) => {
+        if (body.error) {
+            getAccessToken(refreshToken)
+            .then( token => {
+                console.log(token)
+            })
+        }
+    })
+}
+
+// Request access token using refresh token
+const getAccessToken = refreshToken => {
+    const authOptions = {
+        url: "https://accounts.spotify.com/api/token",
+        headers: { Authorization: 
+            "Basic " + new Buffer.from(client_id + ":" + client_secret, 'utf8').toString('base64')
+        },
+        form: { grant_type: "refresh_token", refresh_token: refreshToken },
+        json: true
+    }
+
+    request.post(authOptions, (err, res, body) => {
+        if (!err && res.statusCode === 200) {
+            refreshedAccessToken = body.access_token;
+            return refreshedAccessToken
+        } else {
+            return body
+        }
+    })
+}
+
 module.exports = {
-    ranString: ranString
+    ranString: ranString,
+    verifyToken: verifyToken
 }
