f58def342a878a0b5125dba9c90fa4073089885d
benchan777 <benchan777@gmail.com>
2021-07-06 10:27:14 -0700

    add test route

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -2,8 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 
+const middleware = require('../middleware')
+
 router.get("/", (req, res) => {
     res.render("home");
+});
+
+router.get("/playlist", middleware.verifyToken, (req, res) => {
+    res.render("playlist");
 });
 
 module.exports = router;
