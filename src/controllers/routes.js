56c5dfaf93322b97bc914408c1cd4f9580760913
Jackie <taekwondo.chan@gmail.com>
2021-07-07 16:09:48 -0700

    add refreshtoken check on create_playlist

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -15,60 +15,63 @@ router.get("/create_playlist", middleware.verifyToken, (req, res) => {
 });
 
 router.post("/create_playlist", middleware.verifyToken, (req, res) => {
-	let playlistOptions = {
-		url: `https://api.spotify.com/v1/users/${req.user_id}/playlists`,
-		headers: {
-			Authorization: "Bearer " + req.accessToken,
-			"Content-Type": "application/json",
-		},
-		body: {
-			name: req.body.name,
-			description: req.body.description,
-			public: false,
-		},
-		json: true,
-	};
+	if (req.noRefreshToken) {
+		res.redirect("/login");
+	} else {
+		let playlistOptions = {
+			url: `https://api.spotify.com/v1/users/${req.user_id}/playlists`,
+			headers: {
+				Authorization: "Bearer " + req.accessToken,
+				"Content-Type": "application/json",
+			},
+			body: {
+				name: req.body.name,
+				description: req.body.description,
+				public: false,
+			},
+			json: true,
+		};
 
-	request.post(playlistOptions, (err, response, body) => {
-		let newPlaylist = new Playlist({
-			playlistId: body.id,
-			userId: req.user_id,
-			refreshToken: req.accessToken,
-			name: body.name,
-			description: body.description,
-			collaborative: body.collaborative,
-		});
-
-		newPlaylist
-			.save()
-			.then(() => {
-				return Playlist.find({ playlistId: body.id }).lean();
-			})
-			.then((playlist) => {
-				console.log(playlist);
-				res.render("playlist_details", { playlist });
+		request.post(playlistOptions, (err, response, body) => {
+			let newPlaylist = new Playlist({
+				playlistId: body.id,
+				userId: req.user_id,
+				refreshToken: req.accessToken,
+				name: body.name,
+				description: body.description,
+				collaborative: body.collaborative,
 			});
-	});
+
+			newPlaylist
+				.save()
+				.then(() => {
+					return Playlist.find({ playlistId: body.id }).lean();
+				})
+				.then((playlist) => {
+					console.log(playlist);
+					res.render("playlist_details", { playlist });
+				});
+		});
+	}
 });
 
 router.get("/search_autocomplete", middleware.verifyToken, (req, res) => {
-    if (req.noRefreshToken) {
-        res.redirect('/login')
-    } else {
-        res.render("search_autocomplete", { accessToken: req.accessToken })
-    }
-})
+	if (req.noRefreshToken) {
+		res.redirect("/login");
+	} else {
+		res.render("search_autocomplete", { accessToken: req.accessToken });
+	}
+});
 
 router.post("/search_autocomplete", middleware.verifyToken, (req, res) => {
-    // Example loop to retrieve artist ID
-    const artistList = req.body.name
-    for (let i = 0; i < artistList.length; i++) {
-        middleware.getArtistId(req.accessToken, artistList[i])
-        .then(id => {
-            console.log(id)
-        })
-    }
-    res.render("home")
-})
+	// Example loop to retrieve artist ID
+	const artistList = req.body.name;
+	for (let i = 0; i < artistList.length; i++) {
+		middleware.getArtistId(req.accessToken, artistList[i]).then((id) => {
+			console.log(id);
+		});
+	}
+	res.render("home");
+});
 
 module.exports = router;
