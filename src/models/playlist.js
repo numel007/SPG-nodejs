5ccc3299183cac95af526372999bdf5eeeaaa5b5
Jackie <taekwondo.chan@gmail.com>
2021-07-07 13:40:19 -0700

    create playlist model

---
+++ src/models/playlist.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PlaylistSchema = new Schema({
+	playlistId: { type: String, required: true },
+	userId: { type: String, required: true },
+	refreshToken: { type: String, required: true },
+	name: { type: String, required: true },
+	description: { type: String, required: false },
+});
+
+module.exports = mongoose.model("Playlist", PlaylistSchema);
