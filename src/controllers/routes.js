17d795ffae4806b81af4b1b537f3d1572a5ef288
benchan777 <benchan777@gmail.com>
2021-07-14 23:10:22 -0700

    add proper promise rejects to all functions and catch all errors in create_playlist route

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -22,23 +22,28 @@ router.post("/create_playlist", middleware.verifyToken, async (req, res) => {
 	if (req.noRefreshToken) {
 		res.redirect("/login");
 	} else {
-		const playlistId = await helpers.createPlaylist(req.accessToken, req.cookies.refreshToken, req.user_id, req.body.playlistName, req.body.Description)
+		try {
+			const playlistId = await helpers.createPlaylist(req.accessToken, req.cookies.refreshToken, req.user_id, req.body.playlistName, req.body.Description)
 
-		// Parse seed artists
-		const artistIds = await middleware.getArtistId(req.accessToken, req.body.artistNames);
+			// Parse seed artists
+			const artistIds = await helpers.getArtistId(req.accessToken, req.body.artistNames);
 
-		// Call recommendation endpoint w/ artistIds
-		const recommendUris = await helpers.getRecommendations(artistIds, req.accessToken);
+			// Call recommendation endpoint w/ artistIds
+			const recommendUris = await helpers.getRecommendations(artistIds, req.accessToken);
 
-		// Now clear the playlist
-		await helpers.clearPlaylist(playlistId, req.accessToken);
+			// Now clear the playlist
+			await helpers.clearPlaylist(playlistId, req.accessToken);
 
-		// Then add recommendIds to playlist
-		await helpers.addSongToPlaylist(playlistId, req.accessToken, recommendUris[0]);
+			// Then add recommendIds to playlist
+			await helpers.addSongToPlaylist(playlistId, req.accessToken, recommendUris[0]);
 
-		// Then get newly created playlist's details to be displayed on webpage
-		const playlistDetails = await helpers.getCurrentPlaylistDetails(req.accessToken, req.cookies.refreshToken);
-		return res.send(playlistDetails)
+			// Then get newly created playlist's details to be displayed on webpage
+			const playlistDetails = await helpers.getCurrentPlaylistDetails(req.accessToken, req.cookies.refreshToken);
+			return res.send(playlistDetails)
+		} catch (error) {
+			console.log(error)
+			return res.send(error)
+		}
 	}
 });
 
